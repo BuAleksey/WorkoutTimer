5114882da49f73a77da9641cb277247085f4a49d
Aleksey <oldf03@gmail.com>
2023-09-09 09:51:43 +0300

    Initial Commit

---
+++ WorkoutTimer/WorkoutTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTimerApp.swift
+//  WorkoutTimer
+//
+//  Created by Buba on 09.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
