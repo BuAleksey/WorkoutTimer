277a8ec67be01f892ed049aace609c9a2e055804
Aleksey <oldf03@gmail.com>
2023-11-15 23:18:11 +0300

    make navigation in app

---
+++ WorkoutTimer/Views/MainViewNew.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewNew.swift
+//  WorkoutTimer
+//
+//  Created by Buba on 15.11.2023.
+//
+
+import SwiftUI
+
+struct MainViewNew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainViewNew()
+}
---
+++ WorkoutTimer/Views/Scenes/SetupWorkoutView/SubViews/SelectionView.swift
@@ -0,0 +1,66 @@
+//
+//  SelectionView.swift
+//  WorkoutTimer
+//
+//  Created by Buba on 13.09.2023.
+//
+
+import SwiftUI
+
+struct SelectionView: View {
+    @Binding var timeMinutes: Int
+    @Binding var timeSeconds: Int
+    
+    let title: String
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+            VStack(spacing: -10) {
+                HStack(spacing: 32) {
+                    Text("min")
+                    Text("sec")
+                }
+                .font(.system(size: 15, weight: .light, design: .rounded))
+                HStack(spacing: -15) {
+                    PickerView(
+                        choiceNumber: $timeMinutes,
+                        range: 0...60
+                    )
+                    PickerView(
+                        choiceNumber: $timeSeconds,
+                        range: 0...60
+                    )
+                }
+            }
+        }
+    }
+}
+
+struct PickerView: View {
+    @Binding var choiceNumber: Int
+    var range = 1...100
+    
+    var body: some View {
+        Picker("", selection: $choiceNumber) {
+            ForEach(range, id: \.self) { number in
+                Text(number.formatted())
+                    .foregroundColor(Color("ActionColor"))
+                    .font(.system(size: 15, weight: .bold, design: .rounded))
+            }
+        }
+        .pickerStyle(WheelPickerStyle())
+        .frame(width: 70, height: 80)
+        .clipped()
+    }
+}
+
+extension UIPickerView {
+    open override var intrinsicContentSize: CGSize {
+        return CGSize(
+            width: UIView.noIntrinsicMetric,
+            height: UIView.noIntrinsicMetric
+        )
+    }
+}
